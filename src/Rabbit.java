b8379fb8ee0acb52df70d249a351a802deb17bf8
MohamedH782 <152233933+MohamedH782@users.noreply.github.com>
2023-12-10 20:11:49 +0200

    Rabbit Generating

---
+++ src/Rabbit.java
@@ -0,0 +1,39 @@
+
+
+import com.sun.opengl.util.Animator;
+import com.sun.opengl.util.FPSAnimator;
+
+import javax.media.opengl.GLCanvas;
+import javax.swing.*;
+import java.awt.*;
+
+public class Rabbit extends JFrame {
+    public static void main(String[] args) {
+
+        new Rabbit();
+    }
+    RabbitListener listener = new RabbitGLEventListener();
+
+    public Rabbit() {
+        GLCanvas glcanvas;
+        Animator animator;
+
+
+        glcanvas = new GLCanvas();
+        glcanvas.addGLEventListener(listener);
+        glcanvas.addMouseListener(listener);
+        glcanvas.addMouseMotionListener(listener);
+        getContentPane().add(glcanvas, BorderLayout.CENTER);
+        animator = new FPSAnimator(15);
+        animator.add(glcanvas);
+        animator.start();
+
+        setTitle("Catch The rabbit!!");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(700, 700);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setFocusable(true);
+        glcanvas.requestFocus();
+    }
+}
